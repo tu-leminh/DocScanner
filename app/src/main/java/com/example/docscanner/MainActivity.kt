9a5c63f5f1f58b262716888d94da791d8f0bcd1a
tu-leminh <mt.the.dev@gmail.com>
2022-01-27 18:03:20 +0700

    Edit config

--- app/src/main/java/com/example/docscanner/MainActivity.kt
+++ app/src/main/java/com/example/docscanner/MainActivity.kt
@@ -1,13 +1,36 @@
 package com.example.docscanner
 
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.example.docscanner.databinding.ActivityMainBinding
 import org.opencv.android.OpenCVLoader
+import org.opencv.android.Utils
+import org.opencv.core.Mat
+import org.opencv.imgproc.Imgproc
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        OpenCVLoader.initDebug()
+    }
+
+    fun makeGray(bitmap: Bitmap) : Bitmap {
+        // Create OpenCV mat object and copy content from bitmap
+        val mat = Mat()
+        Utils.bitmapToMat(bitmap, mat)
+
+        // Convert to grayscale
+        Imgproc.cvtColor(mat, mat, Imgproc.COLOR_RGB2GRAY)
+
+        // Make a mutable bitmap to copy grayscale image
+        val grayBitmap = bitmap.copy(bitmap.config, true)
+        Utils.matToBitmap(mat, grayBitmap)
+        return grayBitmap
     }
 }
