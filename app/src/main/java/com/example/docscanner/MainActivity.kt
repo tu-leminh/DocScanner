bb75fc68f4ecf3cc39163dcf77d1788c3af351ce
NXV5111 <hochochoc135@gmail.com>
2022-01-28 10:22:13 +0700

    add intent and request permission

--- app/src/main/java/com/example/docscanner/MainActivity.kt
+++ app/src/main/java/com/example/docscanner/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.example.docscanner
 
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.toBitmap
 import androidx.navigation.fragment.NavHostFragment
 import com.example.docscanner.databinding.ActivityMainBinding
 import org.opencv.android.OpenCVLoader
-
+import java.util.ArrayList
 
 
 class MainActivity : AppCompatActivity() {
@@ -22,6 +29,7 @@ class MainActivity : AppCompatActivity() {
         //binding.imageView.setOnClickListener { change() }
         OpenCVLoader.initDebug()
         ////
+        RequestPermission()
 
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
@@ -33,5 +41,20 @@ class MainActivity : AppCompatActivity() {
 //        bitmap = pp.tf(bitmap)
 //        binding.imageView.setImageBitmap(bitmap)
 //    }
-
+    fun RequestPermission() {
+        var permissions = ArrayList<String>()
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            permissions.add(Manifest.permission.CAMERA)
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        }
+        val arrayPermissions = arrayOfNulls<String>(permissions.size)
+        if (!permissions.isEmpty()) {
+            ActivityCompat.requestPermissions(this, permissions.toArray(arrayPermissions), 100)
+        }
+    }
 }
